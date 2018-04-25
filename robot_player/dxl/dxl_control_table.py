080c1d805307dbdda2c6412817af491bc13b8b55
Tyler McCown <tylermccown@engineering.ucla.edu>
2018-04-24 23:59:14 -0700

    updated the control table to include MX28 2.0 and changed old class to MX28_P1

--- robot_player/dxl/dxl_control_table.py
+++ robot_player/dxl/dxl_control_table.py
@@ -300,6 +300,77 @@ class MX106_P1:
 class MX28:
     """
     DXL Control Table
+    ================
+    Register values from the online manual.
+    Reference:
+    http://support.robotis.com/en/product/actuator/dynamixel/mx_series/mx-28(2.0).htm
+    """
+
+    resolution = 4096
+
+    # =====================================================================
+    # EEPROM
+    # =====================================================================
+    MODEL_NUMBER = 0
+    MODEL_INFORMATION = 2
+    VERSION_OF_FIRMWARE = 6
+    ID = 7
+    BAUD_RATE = 8
+    RETURN_DELAY_TIME = 9
+    DRIVE_MODE = 10
+    OPERATING_MODE = 11
+    SECONDARY_ID = 12
+    PROTOCOL_VERSION = 13
+    HOMING_OFFSET = 20
+    MOVING_THRESHOLD = 24
+    TEMPERATURE_LIMIT = 31
+    MAX_VOLTAGE_LIMIT = 32
+    MIN_VOLTAGE_LIMIT = 34
+    PWM_LIMIT = 36
+    RESERVED = 38
+    ACCELERATION_LIMIT = 40
+    VELOCITY_LIMIT = 44
+    MAX_POSITION_LIMIT = 48
+    MIN_POSITION_LIMIT = 52
+    SHUTDOWN = 63
+
+    # =====================================================================
+    # RAM
+    # =====================================================================
+    TORQUE_ENABLE = 64
+    LED = 65
+    STATUS_RETURN_LEVEL = 68
+    REGISTERED_INSTRUCTION = 69
+    HARDWARE_ERROR_STATUS = 70
+    VELOCITY_I_GAIN = 76
+    VELOCITY_P_GAIN = 78
+    POSITION_D_GAIN = 80
+    POSITION_I_GAIN = 82
+    POSITION_P_GAIN = 84
+    FEEDFORWARD_2ND_GAIN = 88
+    FEEDFORWARD_1ST_GAIN = 90
+    BUS_WATCHDOG = 98
+    GOAL_PWM = 100
+    RESERVED = 102
+    GOAL_VELOCITY = 104
+    PROFILE_ACCELERATION = 108
+    PROFILE_VELOCITY = 112
+    GOAL_POSITION = 116
+    REALTIME_TICK = 120
+    MOVING = 122
+    MOVING_STATUS = 123
+    PRESENT_PWM = 124
+    PRESENT_LOAD = 126
+    PRESENT_VELOCITY = 128
+    PRESENT_POSITION = 132
+    VELOCITY_TRAJECTORY = 136
+    POSITION_TRAJECTORY = 140
+    PRESENT_INPUT_VOLTAGE = 144
+    PRESENT_TEMPERATURE = 146
+
+class MX28_P1:
+    """
+    DXL Control Table
     =================
     Relevant register values from the online manual.
     Reference:
