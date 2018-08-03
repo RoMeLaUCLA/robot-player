7155faf6f1ee8f6e5959d70dbc701f1294aff020
Tyler McCown <tylermccown@engineering.ucla.edu>
2018-08-03 16:09:14 -0700

    fixed another import

--- robot_player/vrep_interface.py
+++ robot_player/vrep_interface.py
@@ -10,7 +10,7 @@ __status__ = "Prototype"
   This class is used to handle the communication between python and V-REP
 '''
 
-from robot_player.vrep import vrep
+from vrep import vrep
 from collections import OrderedDict
 
 class VrepOptions(object):
