1761991296282d7c2052b027ebee3d6ce4a6ceec
Daniel Sun <sundaniel3@gmail.com>
2019-04-12 20:23:57 -0700

    add DxlReadDataError handling example. Copied the TimedLoop class from Peble over to handle this.

---
+++ robot_player/exceptions.py
@@ -0,0 +1,26 @@
+# Daniel Sun 11 Apr 2019 
+# robot-player
+class DxlReadDataError(Exception):
+    """
+    Raise this when data packets are incorrect
+    """
+    pass
+
+
+def read_data():
+    print("beginning of function")
+
+    print('reading data')
+
+    raise DxlReadDataError
+
+    print('ending function')
+
+try:
+    read_data()
+except DxlReadDataError:
+    print("catching exception here")
+finally:
+    print('ending script')
+
+print('script is over')
